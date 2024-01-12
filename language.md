a6eecdd2d0dc52be7634477b48e50e37290ac81d
King Emhyr <kinglanchester@gmail.com>
2024-01-12 11:32:02 -0500

    input to action transformer

---
+++ language.md
@@ -0,0 +1,81 @@
+# Language
+
+## Libraries
+
+There inexists a traditional standard library. Use platform-specific
+procedures.
+
+```
+win32 :: #import "User32.lib" "Kernel32.lib";
+```
+
+# Declarations
+
+## Scope Declaration
+
+```
+scope-declaration
+    : identifier '{' <declaration> '}'
+    ;
+```
+
+A scope declaration declares a collection of symbols.
+
+Moving in and out of scopes to retrieve:
+
+```
+..out1.in1;
+...out2.in1.in2;
+....out3.in1.in2.in3;
+```
+
+## Storage Declarations
+
+```
+storage-declaration
+    : identifier ':' path
+    ;
+```
+
+When you declare something, the type can be any identifier. The compiler
+extracts the type of the identifier to use as the type of the declared objects,
+which means, you don't necessarily declare types; you only declare values.
+
+Using an equals sign or braces to define a declaration means the object has
+runtime storage. Essentially, you can retrieve the runtime address of the
+object.
+
+Using a colon to define a declaration means it only has compiletime storage.
+
+```
+variable: u8 = 0;
+array: {u8*3} = {1, 2, 3};
+enumeration: {sun, mon, tue} = .sun;
+record: {x: f64, y: f64, z: f64} = {x=0,y=0,z=0};
+procedure: (n: u8) -> u8 { -> n; }
+procedure_pointer: @(n: u8) -> u8 = @d;
+CONSTANT: u8 : 32;
+Record :: {r: u8, g: u8, b: u8};
+using_big_record: Record = {r=0,g=0,b=0};
+using_small_record: record = {r=0,g=0,b=0};
+
+x ? y() : z();
+x ? {} : {};
+
+week_day ?
+    | .sun: do_sunday(),
+    | .mon: do_monday(),
+    | .tue: do_tuesday(),
+    : do_somthing();
+
+week_day ?
+    | .sun {}
+    | .mon {}
+    | .tue {}
+    : {}
+
+{4, 2, 3, 1, 2}
+    |> enumerate
+    |> sort;
+   
+```
--- source/slash.cpp
+++ source/slash.cpp
@@ -1,12 +1,189 @@
 #include "slash.h"
 
+void lex();
+void parse();
+void verify();
+void generate();
+void report();
+
 int main(int argc, char **argv)
 {
 	if (argc == 1) FUCK(-1, "Not enough arguments");
-	
+
+	lex();
+	parse();
+	verify();
+	generate();
+	report();
+
 	return 0;
 }
 
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u64 = uint64_t;
+
+void copy(u64 c, char *d, const char *s) {
+	__builtin_memcpy(d, s, c);
+}
+
+enum class Error {
+	redeclaration,
+};
+
+void err(Error error);
+
+enum class Token_Type : u16 {
+	error = 0,
+	identifier = 1,
+	string = 2,
+	floating_point = 3,
+	signed_integer = 4,
+	unsigned_integer = 5,
+	colon = ':',
+	equals_sign = '=',
+	left_curly_bracket = '{',
+	period = '.',
+};
+
+struct Token {
+	u64 length;
+	union {
+		const char *string;
+	};
+	Token_Type type;
+
+	bool is_numeric() {
+		return type >= Token_Type::floating_point
+			&& type <= Token_Type::unsigned_integer;
+	}
+
+	bool is_binary_operator() {
+		switch (type) {
+		default: return false;
+		}
+	}
+};
+
+struct Parser {
+	Token token;
+
+	void lex();
+};
+
+/*
+
+## Alias the contents of "User32.lib"
+win32 :: #import "User32.lib";
+
+## Make an instance of "User32.lib"
+win32_2 := #import "User32.lib";
+
+*/
+
+struct Path {
+};
+
+struct Scope {
+	u8 type:1;
+};
+
+using Identifier = char[64];
+
+struct Declaration_Data {
+	Path type;
+	Scope scope;
+};
+
+struct Declaration {
+	Identifier identifier;
+	Declaration_Data data;
+};
+
+enum class Syntax_Type {
+	declaration,
+	path,
+};
+
+Token current_token;
+Scope *current_scope;
+
+void skip_to(Token_Type);
+
+void expect(Token_Type);
+void expect(Token_Type, Token_Type);
+void expect(Token_Type, Token_Type, Token_Type);
+
+void parse_path(Path *path, bool omittable);
+
+void parse_runtime_scope(Scope *scope) {
+	// expression|procedure
+	lex();
+	if (current_token.is_numeric()) {
+		lex();
+		if (current_token.is_binary_operator()) {
+			lex();
+		}
+		else {
+		}
+	}
+	else if (current_token.type == Token_Type::string) {
+	}
+	else if (current_token.type == Token_Type::identifier) {
+	}
+	else if (current_token.type == Token_Type::period) {
+	}
+	else if (current_token.type == Token_Type::left_curly_bracket) {
+	}
+}
+
+void parse_comptime_scope(Scope *scope) {
+	// expression|type|source
+}
+
+void parse_declaration(Declaration *out) {
+	expect(Token_Type::identifier);
+	copy(current_token.length, out->identifier, current_token.string);
+	expect(Token_Type::colon);
+	parse_path(&out->data.type, true);
+	expect(Token_Type::equals_sign, Token_Type::colon);
+	switch (current_token.type) {
+	case Token_Type::equals_sign: parse_runtime_scope(&out->data.scope); break;
+	case Token_Type::colon:		  parse_comptime_scope(&out->data.scope); break;
+	default: __builtin_unreachable();
+	}
+
+}
+
+struct Program {
+
+};
+
+void parse_program(Program *out) {
+}
+
+template<typename Key, typename Value>
+struct Table {
+	u64 count;
+	Key (*keys)[];
+	Value (*values)[];
+
+	Value *push(Key *key, Value *value);
+	Value *get(Key *key);
+};
+
+Table<char[64], Declaration_Data> *current_declarations;
+
+bool parse_program(Declaration *out) {
+	bool result = true;
+	Declaration declaration;
+	for (;;) {
+		parse_declaration(&declaration);
+	}
+	return result;
+}
+
+#if 0
 void v_report(const char *fn, unsigned line, report_level_t lvl, const char *fmt, va_list vargs)
 {
 	char buf[512];
@@ -52,3 +229,4 @@ void fuck(const char *fn, unsigned line, int code, const char *fmt, ...)
 	va_end(vargs);
 	exit(code);
 }
+#endif
